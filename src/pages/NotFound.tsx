c9911f5fee97f1d50f8ff066c21d0b1b49c5fba7
Shubhanshu Saxena <shubhanshu.e01@gmail.com>
2021-10-02 02:53:00 +0530

    Add 404 Component
    Signed-off-by: Shubhanshu Saxena <shubhanshu.e01@gmail.com>

---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,80 @@
+import Lottie from 'react-lottie';
+import { useAuth } from '../context/auth';
+import { logOutOutline, logoGoogle } from 'ionicons/icons';
+import animationData from '../lottie/404.json';
+import {
+    IonContent,
+    IonHeader,
+    IonPage,
+    IonButtons,
+    IonTitle,
+    IonButton,
+    IonIcon,
+    IonToolbar,
+    IonMenuButton,
+    IonCol,
+    IonRow,
+    IonLabel,
+} from '@ionic/react';
+
+const NotFound = () => {
+    const { handleSignIn, user, handleLogout } = useAuth();
+    return (
+        <IonPage>
+            <IonHeader>
+                <IonToolbar>
+                    <IonButtons slot="start">
+                        <IonMenuButton />
+                    </IonButtons>
+                    <IonTitle>Page Not Found</IonTitle>
+                    {user ? (
+                        <>
+                            <IonButtons slot="end">
+                                <span>Hi, {'Shubhanshu'}!</span>
+                                <IonButton
+                                    onClick={async () => await handleLogout()}
+                                >
+                                    <IonIcon
+                                        slot="icon-only"
+                                        icon={logOutOutline}
+                                    ></IonIcon>
+                                </IonButton>
+                            </IonButtons>
+                        </>
+                    ) : (
+                        <IonButtons slot="end">
+                            <IonButton
+                                slot="start"
+                                onClick={async () => await handleSignIn()}
+                            >
+                                Login with&nbsp;
+                                <IonIcon icon={logoGoogle} />
+                            </IonButton>
+                        </IonButtons>
+                    )}
+                </IonToolbar>
+            </IonHeader>
+            <IonContent>
+                <IonRow className="ion-text-center">
+                    <IonCol>
+                        <Lottie
+                            options={{
+                                loop: true,
+                                autoplay: true,
+                                animationData: animationData,
+                                rendererSettings: {
+                                    preserveAspectRatio: 'xMidYMid slice',
+                                },
+                            }}
+                            height={200}
+                            width={250}
+                        />
+                        <IonLabel>Page Not Found!</IonLabel>
+                    </IonCol>
+                </IonRow>
+            </IonContent>
+        </IonPage>
+    );
+};
+
+export default NotFound;
